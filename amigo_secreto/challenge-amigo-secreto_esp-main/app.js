117b5cf0fcab45ab010d72ea26964885bb4fff4c
Jass <ixcheeljasminhuertaramos@gmail.com>
2025-09-09 23:40:26 -0600

    creacion de funciones
    1. insertar amigo
    2. lista amigos
    3. sorteo

--- amigo_secreto/challenge-amigo-secreto_esp-main/app.js
+++ amigo_secreto/challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,68 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//array 
+let amigos =[]; 
+
+//funcion insertar amigo 
+function agregarAmigo(nombre)
+{
+    let nombre_amigo= document.getElementById('amigo').value;
+
+    if(nombre_amigo != "")
+    {
+         amigos.push(nombre_amigo);
+        document.getElementById('amigo').value="";
+        
+    }
+    else 
+    {
+       alert("Por favor, inserte un nombre.");
+    }
+
+   // document.getElementById('amigo').innerHTML="";
+
+   listaAmigos(); 
+}
+
+//funcion para lista de amigos 
+function listaAmigos()
+{
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML=""; 
+    
+    for(let i=0; i<amigos.length; i++)
+    {
+        let item= document.createElement('li'); 
+        item.textContent= amigos[i]; 
+        lista.appendChild(item);
+    }
+    return lista; 
+
+}
+
+
+//Funcion sorteo
+function sorteo()
+{
+    if(amigos.length=== 0)
+    {
+        alert("no hay smigos en el arreglo"); 
+        return; 
+    }
+    if(amigos.length!=0)
+    {
+    let res= Math.floor(Math.random()*amigos.length); 
+
+    let amigoElemento= document.getElementById('resultado');
+    amigoElemento.innerHTML= "";
+
+    let item = document.createElement('li');
+    item.textContent = "Tu amigo secreto es: " + amigos[res];
+   amigoElemento.appendChild(item);
+
+
+    
+
+    }
+
+}
